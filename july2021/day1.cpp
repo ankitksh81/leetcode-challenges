1d3668cd531d5c64425ce076f46fcad825af791a
ankitksh81 <ankitksh81@gmail.com>
2021-07-02 02:06:35 +0530

    July 2021 Day1 solution

---
+++ july2021/day1.cpp
@@ -0,0 +1,16 @@
+/* Day 1
+    Question link : https://leetcode.com/explore/featured/card/july-leetcoding-challenge-2021/608/week-1-july-1st-july-7th/3799/
+*/
+
+// Solution:
+
+class Solution{
+public:
+    vector<int> grayCode(int n) {
+        vector<int> ans;
+        for(int i = 0; i < (1 << n); i++) {
+            ans.push_back(i ^ (i >> 1));
+        }
+        return ans;
+    }
+};
