f6c4685117246303c44fe25b4058eeb9b45db063
ankitksh81 <ankitksh81@gmail.com>
2021-10-03 16:48:05 +0530

    day 3 sol

---
+++ October 2021/day3.cpp
@@ -0,0 +1,22 @@
+/*
+    Jump Game
+    https://leetcode.com/problems/jump-game/
+*/
+
+// Solution:
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int can_reach = 0;
+        for(int i = 0; i <= can_reach; i++) {
+            if(i == n-1) {
+                return true;
+            }
+            can_reach = max(can_reach, i + nums[i]);
+        }
+
+        return false;
+    }
+};
