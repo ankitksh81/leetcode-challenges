f14ce1daf6c01790b7d5ebfc9718eb0ad92f248c
ankitksh81 <ankitksh81@gmail.com>
2022-02-07 21:46:05 +0530

    day 7 sol

---
+++ February 2022/day7.cpp
@@ -0,0 +1,20 @@
+/*
+	Find the Difference
+	https://leetcode.com/problems/find-the-difference/
+*/
+
+// Solution: Xor
+
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char x = 0;
+        for(char c: s) {
+            x ^= c;
+        }
+        for(char c: t) {
+            x ^= c;
+        }
+        return x;
+    }
+};
