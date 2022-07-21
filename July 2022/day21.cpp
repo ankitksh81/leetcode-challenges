be7c67853f50f07359d0fcbb4093e6b7909ad566
ankitksh81 <ankitksh81@gmail.com>
2022-07-21 16:02:27 +0530

    day 21 sol

--- July 2022/day21.cpp
+++ July 2022/day21.cpp
@@ -1,15 +1,10 @@
 /*
 	Reverse Linked List II
-	/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+    https://leetcode.com/problems/reverse-linked-list-ii/
+*/
+
+// Solution: Iterative
+
 class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
