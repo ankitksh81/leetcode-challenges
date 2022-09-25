2300a8c373e8c1a2cff532aca9012edfe46d0798
Ankit Kumar <55939152+ankitksh81@users.noreply.github.com>
2022-09-25 07:28:47 +0530

    week 4 sol

---
+++ September 2022/week4.cpp
@@ -0,0 +1,29 @@
+/*
+        Remove Interval
+        https://leetcode.com/problems/remove-interval/
+*/
+
+// Solution: Greedy
+
+class Solution {
+public:
+    vector<vector<int>> removeInterval(vector<vector<int>>& intervals, vector<int>& toBeRemoved) {
+        vector<vector<int>> result;
+        
+        for (vector<int>& interval : intervals) {
+            if (interval[1] <= toBeRemoved[0] || interval[0] >= toBeRemoved[1]) {
+                result.push_back(interval);
+            }
+            else {
+                if (interval[0] < toBeRemoved[0]) {
+                    result.push_back({interval[0] , toBeRemoved[0]});
+                }
+                if (interval[1] > toBeRemoved[1]) {
+                    result.push_back({toBeRemoved[1], interval[1]});
+                }
+            }
+        }
+        
+        return result;
+    }
+};
